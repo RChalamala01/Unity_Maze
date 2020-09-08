7ba56ad614609414d6902aaa0fe0824913ce1549
Noah Conner <nconner2@illinois.edu>
2020-09-08 14:49:42 -0500

    script for creating n*m-size maze

---
+++ Maze/Assets/Scripts/MazeBuilder.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MazeBuilder : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public GameObject tilePrefab;
+    public Transform tileParent;
+
+    public int width;
+    public int height;
+    public GameObject[,] tileMatrix;
+
+
+    void Start()
+    {
+        tileMatrix = new GameObject[height, width];
+        cam.transform.position = new Vector3((width - 1) * 0.55f, Mathf.Max(height, width), (height - 1) * 0.55f);
+        InstantiateMaze(height, width);
+    }
+
+    void InstantiateMaze(int rows, int columns)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(j * 1.1f, 0, i * 1.1f), Quaternion.identity, tileParent);
+                tile.transform.name = "tile (" + i + ", " + j + ")";
+                tileMatrix[i, j] = tile;
+            }
+        }
+
+    }
+
+}
